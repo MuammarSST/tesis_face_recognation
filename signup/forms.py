135a1199837086de904ff84957294401de8a1127
Muammar, SST <52598691+MuammarSST@users.noreply.github.com>
2022-03-27 12:40:12 +0700

    pembuatan registrasi dan login tahap ke 3

--- signup/forms.py
+++ signup/forms.py
@@ -5,17 +5,3 @@ class BiodataUser(forms.ModelForm):
     class Meta:
         model=Data_user
         fields="__all__"
-        error_messages = {
-            'firstname':{
-                'required':'Anda Harus Mengisi Nama Depan'
-            },
-            'lastname':{
-                'required':'Anda Harus Mengisi Nama Belakang'
-            },
-            'email':{
-                'required':'Anda Harus Mengisi Email'
-            },
-            'password':{
-                'required':'Anda Harus Mengisi Password'
-            }
-        }
--- signup/views.py
+++ signup/views.py
@@ -1,47 +1,42 @@
-
+import mysql.connector
 from django.shortcuts import render, redirect, get_object_or_404
 
+from hashlib import md5
 
-# import mysql.connector as sql
-# fn=''
-# ln=''
-# s=''
-# em=''
-# pwd=''
-
-
-
-# Create your views here.
-# def signAction(request):
-#     global fn,ln,s,em,pwd
-#     if request.method=="POST":
-#         m=sql.connect(host="localhost",user="root",passwd="aspire4730z",database="presensi")
-#         cursor=m.cursor()
-#         d=request.POST
-#         for key,value in d.items():
-#             if key=="firstname":
-#                 fn=value
-#             if key=="lastname":
-#                 ln=value
-#             if key=="sex":
-#                 s=value
-#             if key=="email":
-#                 em=value
-#             if key=="password":
-#                 pwd=value
-#
-#         c="INSERT INTO `presensi`.`users` (`firstname`, `lastname`, `sex`, `email`, `password`) VALUES (fn, ln, s, em, MD5(pwd))"
-#         cursor.execute(c)
-#         m.commit()
-#     return render (request,'signup_page.html')
 from signup.forms import BiodataUser
 
 
+
 def signAction(request):
-    form=BiodataUser(request.POST or None, request.FILES or None)
+    form=BiodataUser(request.POST)
     if request.method=='POST':
         if form.is_valid():
-            form.save()
+
+            firstname = form.cleaned_data['firstname']
+            lastname = form.cleaned_data['lastname']
+            sex = form.cleaned_data['sex']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['email']
+            password_hash = md5( password.encode("utf-8") ).hexdigest()
+
+
+
+
+            mydb  = mysql.connector.connect(
+                user='root',
+                password='aspire4730z',
+                host='127.0.0.1',
+                database='presensi'
+            )
+            mycursor = mydb.cursor()
+            sql = (
+                "INSERT INTO users(firstname, lastname, sex, email, password)"
+                "VALUES (%s, %s, %s, %s, %s)"
+            )
+            val = (firstname, lastname, sex, email, password_hash)
+            mycursor.execute(sql, val)
+            mydb.commit()
+
             return redirect("/")
         pass
     return render(request,"signup_page.html")
